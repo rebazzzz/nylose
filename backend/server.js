795228f7a66a7c61c1c20c3d91715b4d23173493
unknown <rebazrawoof8@gmail.com>
2026-01-03 16:20:50 +0100

    issues with schedule data and more

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 3001;
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || [
+      "http://localhost:3000",
+      "http://localhost:8000",
+    ],
     credentials: true,
   })
 );
